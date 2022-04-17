8dcc5c6a8352f39dfef3898827ef3c191749fa21
carol-profile <carolgitonga93@gmail.com>
2022-04-18 02:47:54 +0300

    make changes

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from './user';
+import {Repository} from './repository';
+import {environment} from '../environments/environment';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+
+@Injectable()
 export class GithubService {
+  username: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log('Github Service Ready...');
+    this.username =  'carol-profile';
+
+  }
+  getUser():Observable<any> {
+    return this.http.get('https://api.github.com/users/' + this.username)
+  }
+  getRepos():Observable<any> {
+    return this.http.get(' https://api.github.com/users/' + this.username + '/repos')
+    
+}
+updateUser(username: string) {
+  this.username = username;
+}
 }
