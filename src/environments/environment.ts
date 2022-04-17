c7f8fb7691e5751b9ea758aac5cf86dd7c72c865
carol-profile <carolgitonga93@gmail.com>
2022-04-17 22:07:09 +0300

    add repository class

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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'
 };
 
 /*
